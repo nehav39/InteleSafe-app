3a5710aa788f5ad82c79f9217154b22ecc8937a0
Deepak Sachdeva <deepak.sachdeva@ad.infosys.com>
2020-04-01 19:09:14 +0530

    UI changes for privacy and signup screen

--- app/src/main/java/org/intelehealth/intelesafe/activities/privacyNoticeActivity/PrivacyNotice_Activity.java
+++ app/src/main/java/org/intelehealth/intelesafe/activities/privacyNoticeActivity/PrivacyNotice_Activity.java
@@ -6,7 +6,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -29,10 +34,11 @@ public class PrivacyNotice_Activity extends AppCompatActivity {
     TextView privacy_textview;
     SessionManager sessionManager = null;
     private boolean hasLicense = false;
-    RadioGroup radiogrp;
-    RadioButton radiobtn;
-    RadioButton radio_acc;
-    RadioButton radio_rej;
+//    RadioGroup radiogrp;
+//    RadioButton radiobtn;
+//    RadioButton radio_acc;
+//    RadioButton radio_rej;
+    CheckBox chbAgreePrivacy;
     TextView txt_next;
 
     @Override
@@ -58,9 +64,11 @@ public class PrivacyNotice_Activity extends AppCompatActivity {
         sessionManager = new SessionManager(this);
         privacy_textview = findViewById(R.id.privacy_text);
 
-        radiogrp = findViewById(R.id.radio_privacy_grp);
-        radio_acc = findViewById(R.id.radio_accept);
-        radio_rej = findViewById(R.id.radio_reject);
+//        radiogrp = findViewById(R.id.radio_privacy_grp);
+//        radio_acc = findViewById(R.id.radio_accept);
+//        radio_rej = findViewById(R.id.radio_reject);
+
+        chbAgreePrivacy = findViewById(R.id.chb_agree_privacy);
 
         txt_next = findViewById(R.id.txt_privacy);
 
@@ -119,35 +127,23 @@ public class PrivacyNotice_Activity extends AppCompatActivity {
 
             } else {
                 String privacy_string = obj.getString("privacyNoticeText");
-                privacy_textview.setText(privacy_string);
+                changeColorOfText(privacy_string);
             }
-
 
             txt_next.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-
-                    int selected_radio = radiogrp.getCheckedRadioButtonId();
-                    radiobtn = findViewById(selected_radio);
-
-                    if (radio_acc.isChecked() || radio_rej.isChecked()) {
-                        if (radio_acc.isChecked()) {
+                    if (chbAgreePrivacy.isChecked()) {
 //                            sessionManager.setOfllineOpenMRSID("");
-                            Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-                            intent.putExtra("privacy", radiobtn.getText()); //privacy value send to identificationActivity
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-                                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(PrivacyNotice_Activity.this, getString(R.string.privacy_reject_toast), Toast.LENGTH_LONG).show();
-                            finish();
-                        }
+                        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                        intent.putExtra("privacy", chbAgreePrivacy.getText()); //privacy value send to identificationActivity
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
                     } else {
                         Toast.makeText(getApplicationContext(), getString(R.string.privacy_toast), Toast.LENGTH_LONG).show();
                     }
-
-
                 }
             });
 
@@ -156,7 +152,26 @@ public class PrivacyNotice_Activity extends AppCompatActivity {
             Crashlytics.getInstance().core.logException(e);
             Toast.makeText(getApplicationContext(), "JsonException" + e, Toast.LENGTH_LONG).show();
         }
+    }
 
+    private void changeColorOfText(String privacy_string) {
+        //Highlighting Text
+        String second = "Your information is used for diagnosing and treating you.";
+        Spannable spannable = colorized(privacy_string, second, Color.BLUE);
+        privacy_textview.setText(spannable, TextView.BufferType.SPANNABLE);
+    }
 
+    public static Spannable colorized(final String text, final String word, final int argb) {
+        final Spannable spannable = new SpannableString(text);
+        int substringStart = 0;
+        int start;
+        while ((start = text.indexOf(word, substringStart)) >= 0) {
+            spannable.setSpan(
+                    new ForegroundColorSpan(argb), start, start + word.length(),
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+            substringStart = start + word.length();
+        }
+        return spannable;
     }
 }
