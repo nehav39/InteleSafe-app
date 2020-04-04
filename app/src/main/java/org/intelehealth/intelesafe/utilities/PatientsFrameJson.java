8ad2ba31062afe4cb7817119b2a373705ed17fc8
prajwalmw <prajwalwaingankar@yahoo.in>
2020-04-04 20:08:38 +0530

    New Logo added on the login screen.

--- app/src/main/java/org/intelehealth/intelesafe/utilities/PatientsFrameJson.java
+++ app/src/main/java/org/intelehealth/intelesafe/utilities/PatientsFrameJson.java
@@ -173,7 +173,7 @@ public class PatientsFrameJson {
         pushRequestApiCall.setVisits(visitList);
         pushRequestApiCall.setEncounters(encounterList);
         Gson gson = new Gson();
-        Log.d("OBS: ","OBS: "+gson.toJson(encounterList));
+        Log.d("OBS: ","OBS: "+gson.toJson(pushRequestApiCall));
 
 
         return pushRequestApiCall;
