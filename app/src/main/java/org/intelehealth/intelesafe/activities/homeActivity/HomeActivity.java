1429c7f4094716c4be208b09314a10c881c1aa3b
Deepak Sachdeva <deepak.sachdeva@ad.infosys.com>
2020-04-07 15:45:26 +0530

    Logout issue, mental health view and visit and home screen UI

--- app/src/main/java/org/intelehealth/intelesafe/activities/homeActivity/HomeActivity.java
+++ app/src/main/java/org/intelehealth/intelesafe/activities/homeActivity/HomeActivity.java
@@ -109,7 +109,7 @@ import io.reactivex.schedulers.Schedulers;
  * Home Screen
  */
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = HomeActivity.class.getSimpleName();
     SessionManager sessionManager = null;
@@ -139,6 +139,7 @@ public class HomeActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     TextView tvNoVisit;
     Button help_watsapp;
+    TextView tvMentalHelpRequest;
 
     Context context;
     private String mindmapURL = "";
@@ -217,6 +218,9 @@ public class HomeActivity extends AppCompatActivity {
                                         phoneNumberWithCountryCode, message))));
             }
         });
+
+        tvMentalHelpRequest = findViewById(R.id.tv_mental_help_request);
+        tvMentalHelpRequest.setOnClickListener(this);
 
         SharedPreferences mSharedPreference = PreferenceManager.getDefaultSharedPreferences(Objects.requireNonNull(context));
         int dayCount = mSharedPreference.getInt("dayCount", 0);
@@ -695,6 +699,16 @@ public class HomeActivity extends AppCompatActivity {
         startActivity(intent2);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_mental_help_request:
+                Uri uri = Uri.parse("https://www.intelehealth.org/mental-health-consult"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+        }
+    }
+
     private class DownloadFile extends AsyncTask<String, Void, Void> {
 
         @Override
@@ -949,9 +963,13 @@ public class HomeActivity extends AppCompatActivity {
         Account[] accountList = manager.getAccountsByType("io.intelehealth.openmrs");
         if (accountList.length > 0) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                manager.removeAccount(accountList[0], HomeActivity.this, null, null);
+                for(int i = 0; i < accountList.length; i++){
+                    manager.removeAccount(accountList[i], HomeActivity.this, null, null);
+                }
             } else {
-                manager.removeAccount(accountList[0], null, null); // Legacy implementation
+                for(int i = 0; i < accountList.length; i++) {
+                    manager.removeAccount(accountList[i], null, null); // Legacy implementation
+                }
             }
         }
 
