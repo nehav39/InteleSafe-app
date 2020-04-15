382e859a5bbf5368d4375c95553b1bed8bdfb98a
Deepak Sachdeva <deepak.sachdeva@ad.infosys.com>
2020-04-15 16:01:36 +0530

    created profile activity

---
+++ app/src/main/java/org/intelehealth/intelesafe/activities/profile/ProfileActivity.java
@@ -0,0 +1,15 @@
+package org.intelehealth.intelesafe.activities.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.intelehealth.intelesafe.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
