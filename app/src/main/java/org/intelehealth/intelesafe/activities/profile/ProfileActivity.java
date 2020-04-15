6ba9bf98145f6fb9f69509a21178c6eda3b33a6e
Deepak Sachdeva <deepak.sachdeva@ad.infosys.com>
2020-04-15 18:48:06 +0530

    profile screen ui

--- app/src/main/java/org/intelehealth/intelesafe/activities/profile/ProfileActivity.java
+++ app/src/main/java/org/intelehealth/intelesafe/activities/profile/ProfileActivity.java
@@ -2,6 +2,8 @@ package org.intelehealth.intelesafe.activities.profile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import org.intelehealth.intelesafe.R;
 
@@ -11,5 +13,11 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
     }
 }
