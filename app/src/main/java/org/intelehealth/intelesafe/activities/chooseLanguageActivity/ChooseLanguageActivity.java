b578eb9cff99b4a5610d2edc835addb55a3422a6
Nishita <goyalnishita07@gmail.com>
2021-06-04 13:30:18 +0530

    Intro activity removed

--- app/src/main/java/org/intelehealth/intelesafe/activities/chooseLanguageActivity/ChooseLanguageActivity.java
+++ app/src/main/java/org/intelehealth/intelesafe/activities/chooseLanguageActivity/ChooseLanguageActivity.java
@@ -18,6 +18,7 @@ import androidx.appcompat.widget.Toolbar;
 import org.intelehealth.intelesafe.R;
 import org.intelehealth.intelesafe.activities.homeActivity.HomeActivity;
 import org.intelehealth.intelesafe.activities.introActivity.IntroActivity;
+import org.intelehealth.intelesafe.activities.loginActivity.LoginActivity;
 import org.intelehealth.intelesafe.utilities.Logger;
 import org.intelehealth.intelesafe.utilities.SessionManager;
 
@@ -85,7 +86,8 @@ public class ChooseLanguageActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (sessionManager.isFirstTimeLaunch()) {
                     Logger.logD(LOG_TAG, "Starting setup");
-                    Intent intent = new Intent(ChooseLanguageActivity.this, IntroActivity.class);
+//                    Intent intent = new Intent(ChooseLanguageActivity.this, IntroActivity.class);
+                    Intent intent = new Intent(ChooseLanguageActivity.this, LoginActivity.class);
                     startActivity(intent);
                 } else {
                     Intent intent = new Intent(ChooseLanguageActivity.this, HomeActivity.class);
